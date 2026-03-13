07363fa8c1ca0353c0705e53b320924eb15963b4
ammarimran125-wq <ammarimran125@gmail.com>
2026-03-13 15:55:43 +0500

    code practice

--- src/break_and_continue.java
+++ src/break_and_continue.java
@@ -1,12 +1,44 @@
 public class break_and_continue {
     public static void main(String[] args) {
-        for(int i=0; i<50; i++){
-            System.out.println(i);
-            System.out.println("Java is great");
+//        for(int i=0; i<50; i++){
+//            System.out.println(i);
+//            System.out.println("Java is great");
+//            if(i==2){
+//                System.out.println("Ending the loop");
+//                break;
+//            }
+//        }
+//        int i = 0;
+//        while(i<5){
+//            System.out.println(i);
+//            System.out.println("Java is great");
+//            if (i == 2){
+//                break;
+//            }
+//            i++;
+//        }
+//        System.out.println("Loop ends here!");
+//    }
+//}
+//        int i = 0;
+//        do{
+//            System.out.println(i);
+//            System.out.println("Java is great");
+//            if (i == 2){
+//                break;
+//            }
+//            i++;
+//        }while (i<5);
+//        System.out.println("Loop ends here!");
+//        usage of continue
+        for(int i=0; i<5; i++) {
             if(i==2){
                 System.out.println("Ending the loop");
-                break;
-            }
+                continue;
         }
+              System.out.println(i);
+            System.out.println("Java is great");
+            }
+       }
     }
-}
+
---
+++ src/ps5.java
@@ -0,0 +1,39 @@
+public class ps5 {
+    public static void main(String[] args) {
+//        practice problem 1
+//        int n = 4;
+//        for (int i=n; i>0; i--){
+//            for(int j=0; j<i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.print("\n");
+//        }
+//        problem no 2
+//        int sum = 0;
+//        int n=4;
+//        for(int i=0; i<n; i++){
+//            sum = sum + (2*i);
+//        }
+//        System.out.print("Sum of even number is: ");
+//        System.out.println(sum);
+//        sum of first 4 even number is _ 0+2+4+6...
+//        problem no:3
+//          int n = 5;
+//          for(int i=10; i>1; i--){
+//              System.out.printf("%d X %d = %d\n" , n, i, n*i);
+//          }
+//          problem 4
+//        int n = 5;
+//        int i = 1;
+//        int factorial = 1;
+//        while (i <= n) {
+//            factorial *= i;
+//            i++;
+//        }
+//        System.out.println(factorial);
+//        problem 5
+
+    }
+}
+
+
